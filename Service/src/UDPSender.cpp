a803bc00b0ba8fd656c036db072a8fb3eb886de0
ElectroStar <sebi1500@web.de>
2014-11-06 12:06:39 +0100

    Datenpaket ins Netzwerk senden per UDP

--- Service/src/UDPSender.cpp
+++ Service/src/UDPSender.cpp
@@ -6,7 +6,44 @@
  */
 
 #include "UDPSender.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "PosData.h"
+#include <iostream>
+#include <string.h>
 
 void UDPSender::run() {
+	// Socket erstellen
+	int sock;
+	if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		cout << "Konnte UDP-Socket nicht erstellen" << endl;
+		exit(EXIT_FAILURE);
+	}
 
+	// Broadcast-Netzwerkadresse holen
+	struct sockaddr_in network;
+	memcpy(&network, _adapter->getBroadcastAddress(), sizeof(*(_adapter->getBroadcastAddress())));
+	// Ziel-Port setzen
+	network.sin_port = htons(_port);
+
+	// TODO: Statt eine Endlosschleife das Beenden aus dem Main Thread ermoeglichen
+	while(1) {
+		// Warten bis Positionsdaten vorliegen, welche weitergeleitet werden sollen
+		PosData data = _queue->pop();
+
+		// PosData in String umwandenln und dann in Char-Array
+		string posDataString = data;
+		char *buffer=new char[posDataString.size()+1];
+		buffer[posDataString.size()]=0;
+		memcpy(buffer,posDataString.c_str(),posDataString.size());
+
+		// Daten ins Netzwerk senden
+		if (sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&network, sizeof(network)) < 0) {
+			cout << "Senden ins Netzwerk fehlgeschlagen" << endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// Socket wieder schließen
+	close(sock);
 }
